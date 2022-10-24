acd764d44b73c9c79cf8cd6876363c918182817c
tajin002 <tajinshawon002@gmail.com>
2022-10-25 03:27:17 +0600

    catagories  path created

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const port = process.env.port || 5000;
 
 app.use(cors());
 
+const categories = require("./Data/education.json");
+
 app.get('/' , (req , res)=>{
     res.send("server is running ");
 });
+
+app.get ("/categories", (req , res)=>{
+    res.send(categories)
+})
 
 app.listen(port,()=>{
     console.log(`server prot ${port}`);
